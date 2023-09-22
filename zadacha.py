c48f70ad221e603bf3ea8e0499b4475445239036
noeuseemo <zuf_x_god@mail.ru>
2023-09-22 23:18:23 +0700

    first commit

---
+++ zadacha.py
@@ -0,0 +1,12 @@
+
+a = int(input('введите число- '))
+b = int(input('введите степень '))
+r = 1
+
+if b > 0:
+        for i in range(b+1):
+            if i == 0:
+                continue
+
+            r*=a
+        print(r)
